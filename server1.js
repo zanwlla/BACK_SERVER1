2c3aa4d6ae649527f6050a97d9d7a4af8c852293
Camila <zanellacamila016@gmail.com>
2024-04-19 08:52:44 -0300

    primeira aula

---
+++ server1.js
@@ -0,0 +1,16 @@
+//19/04- criar o primeiro servidor web usando o node 
+const http= require('http');
+
+const server=http.createServer((req,res)=>{
+    console.log("Efetuando pedido");
+    //declarando um header
+    res.setHeader('content-type,text/html');
+    res.write("<h1>O que vossa senhoria deseja?</h1>");
+    res.write('<h2>Dormir</h2>');
+    res.write('Cafézinho');
+    res.end();
+})
+
+server.listen(3000,'localhost', ()=>{
+    console.log('Servidor no ar!!!');
+})
